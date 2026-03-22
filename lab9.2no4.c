65676ac994be05af0ac27a7d705e977f6c1a982e
s6704062616109-hub <s6704062616109@email.kmutnb.ac.th>
2026-03-22 15:13:10 +0700

    Update lab9.2no4.c

--- lab9.2no4.c
+++ lab9.2no4.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
 int checkLogin(char *login, char *passwd) {
 if(!strcmp(login,"student1") && !strcmp(passwd, "mypass"))
 return 1;
 else
 return 0;
 }
-int main() {
-char login[64], password[64];
-printf("Enter login : "); gets(login);
-printf("Enter password : "); gets(password);
-if( checkLogin(login, password) == 1) {
-printf("Welcome\n");
-} else {
-printf("Incorrect login or password\n");
+
+int checkValidPass41(char *ps) {
+    int len = strlen(ps);
+    int digitCount = 0;
+
+    if (len != 5) return 0;
+
+    for (int i = 0; i < len; i++) {
+        if (isdigit(ps[i])) digitCount++;
+    }
+
+    return (digitCount >= 1);
 }
+
+int main() {
+    char login[64], password[64];
+    printf("Enter login : "); scanf("%s", login);
+    printf("Enter password : "); scanf("%s", password);
+
+    if (checkValidPass41(password)) {
+        printf("Password Valid: Welcome\n");
+    } else {
+        printf("Invalid Password format!\n");
+    }
+    return 0;
 }
